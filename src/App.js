24d534747ca5257f4ec817380c1e98235f78390c
unknown <asobolev91@gmail.com>
2020-10-20 21:02:32 -0700

    header & footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import Main from './components/MainComponent';
 import './App.css';
+
+library.add(fab, far, fas);
 
 class App extends Component {
  
---
+++ src/components/FooterComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Footer(props) {
+	return(
+		<div className="footer">
+        <div className="container">
+            <div className="row justify-content-center">             
+                <div className="col-4 offset-1 col-sm-2">
+                    <h5>Links</h5>
+                    <ul className="list-unstyled">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="contactus.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>
+		              121, Clear Water Bay Road<br />
+		              Clear Water Bay, Kowloon<br />
+		              HONG KONG<br />
+		              <FontAwesomeIcon icon={['fas', 'phone']} size="lg" />: +852 1234 5678<br />
+		              <FontAwesomeIcon icon={['fas', 'fax']} size="lg" />: +852 8765 4321<br />
+		              <FontAwesomeIcon icon={['fas', 'envelope']} size="lg" /> <a href="mailto:confusion@food.net">
+                         confusion@food.net</a>
+                    </address>
+                </div>
+                <div className="col-12 col-sm-4 align-self-center">
+                    <div className="text-center">
+                        <a className="btn" href="http://google.com/+"><FontAwesomeIcon icon={['fab', 'google-plus']} size="2x" /></a>
+                        <a className="btn" href="http://www.facebook.com/profile.php?id="><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" /></a>
+                        <a className="btn" href="http://www.linkedin.com/in/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" /></a>
+                        <a className="btn" href="http://twitter.com/"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" /></a>
+                        <a className="btn" href="http://youtube.com/"><FontAwesomeIcon icon={['fab', 'youtube']} size="2x" /></a>
+                        <a className="btn" href="mailto:"><FontAwesomeIcon icon={['far', 'envelope']} size="2x" /></a>
+                    </div>
+                </div>
+            </div>
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                </div>
+            </div>
+        </div>
+    </div>
+	);
+}
+
+export default Footer;
